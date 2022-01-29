06b468fe1b53ca387e8f2cae437a0c659c8c418a
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-01-29 18:18:16 +0100

    docs version -1
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

---
+++ docs/doxy/html/class_mini_craft_1_1_input_1_1_c_input_handler.js
@@ -0,0 +1,15 @@
+var class_mini_craft_1_1_input_1_1_c_input_handler =
+[
+    [ "CInputHandler", "class_mini_craft_1_1_input_1_1_c_input_handler.html#a3045e7ee31fcbea63c3bcc3a2077a148", null ],
+    [ "printState", "class_mini_craft_1_1_input_1_1_c_input_handler.html#a1f387938414dcf577cc87dd59dd98b52", null ],
+    [ "releaseAll", "class_mini_craft_1_1_input_1_1_c_input_handler.html#a32170092e19bfc68c41e9ae34a71d340", null ],
+    [ "tick", "class_mini_craft_1_1_input_1_1_c_input_handler.html#aa17c5d269037f2827c890ef8907d6f66", null ],
+    [ "toggle", "class_mini_craft_1_1_input_1_1_c_input_handler.html#a54f04b560c28c251022a1983db33f39b", null ],
+    [ "attack", "class_mini_craft_1_1_input_1_1_c_input_handler.html#a5c6589c882eff19327c45bda8e309d1a", null ],
+    [ "down", "class_mini_craft_1_1_input_1_1_c_input_handler.html#a50fa5bcabb4cadedc1bcf9e6abe147b7", null ],
+    [ "keys", "class_mini_craft_1_1_input_1_1_c_input_handler.html#a6bd85e6afcbe1dcef4dac89d951849d0", null ],
+    [ "left", "class_mini_craft_1_1_input_1_1_c_input_handler.html#a3a00f6267f3e376c8347b4abc7f82e54", null ],
+    [ "menu", "class_mini_craft_1_1_input_1_1_c_input_handler.html#a3eeb1c5e7c087948562414188634955c", null ],
+    [ "right", "class_mini_craft_1_1_input_1_1_c_input_handler.html#aa4d72e3e11ca901a65822244bf8261cb", null ],
+    [ "up", "class_mini_craft_1_1_input_1_1_c_input_handler.html#a4ea45f7d1e43ccf12c39ec22ac96427c", null ]
+];
---
+++ docs/doxy/html/class_mini_craft_1_1_input_1_1_c_key.js
@@ -0,0 +1,14 @@
+var class_mini_craft_1_1_input_1_1_c_key =
+[
+    [ "CKey", "class_mini_craft_1_1_input_1_1_c_key.html#a4d4ef938376c007582681a1ccfb2370e", null ],
+    [ "~CKey", "class_mini_craft_1_1_input_1_1_c_key.html#a0d2c8c279c114dbfbc3b0d744d0e61a1", null ],
+    [ "operator!=", "class_mini_craft_1_1_input_1_1_c_key.html#a15a2937aa154ac3fb9f0698476007480", null ],
+    [ "operator==", "class_mini_craft_1_1_input_1_1_c_key.html#a33b19aabe310ebec6049b872fea94ef6", null ],
+    [ "setInputHandler", "class_mini_craft_1_1_input_1_1_c_key.html#ad23aafef0edbca8d3207d13732b780a4", null ],
+    [ "tick", "class_mini_craft_1_1_input_1_1_c_key.html#aa74bf3e82dfbea115de82f9d035ee1be", null ],
+    [ "toggle", "class_mini_craft_1_1_input_1_1_c_key.html#abdcc124491e2135cf2772ae1797ae8e5", null ],
+    [ "absorbs", "class_mini_craft_1_1_input_1_1_c_key.html#a3c88f7f1a83e5ad766e4e877df9d1b72", null ],
+    [ "clicked", "class_mini_craft_1_1_input_1_1_c_key.html#a33d67b824604820434f94057683a4c9e", null ],
+    [ "down", "class_mini_craft_1_1_input_1_1_c_key.html#a64eb27ef38ccd0d084e8fbc46ce22b6a", null ],
+    [ "presses", "class_mini_craft_1_1_input_1_1_c_key.html#a929589f5d1eea6d8ff9f7ec47d229bf1", null ]
+];
